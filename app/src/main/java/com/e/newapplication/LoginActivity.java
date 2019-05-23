98b9dc3419d47e82ac5f32e0a41c148bf430e46b
Softwarica College Lab <softwarica@softwarica.com>
2019-05-23 10:58:35 +0545

    Initial commit

---
+++ app/src/main/java/com/e/newapplication/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.e.newapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import ClientInterface.HeroesAPI;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsername, etPassword;
+    private Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnRegister=findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUser();
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void checkUser(){
+        HeroesAPI heroesAPI=Url.getInstance().create(HeroesAPI.class);
+        String username=etUsername.getText().toString().trim();
+    }
+}
